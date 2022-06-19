542c0d6b62b85046c4d4692d3fa08a9e469e8618
Abhishek Sharma <avisekksarma@gmail.com>
2022-06-19 07:32:54 +0545

    made battery-manager to include status of charge

--- battery-manager.sh
+++ battery-manager.sh
@@ -4,15 +4,17 @@ path_img="/home/abhishek/Downloads/wallpapers/battery.jpg"
 time_limit=7000
 lower_limit=45
 upper_limit=80
-battery=$(cat /sys/class/power_supply/BAT0/capacity)
-if [[ "$battery" -gt 98 ]]; then
+path="/sys/class/power_supply/BAT0/"
+battery="$(cat $path/capacity)"
+status="$(cat $path/status)"
+if [[ "$battery" -gt 98 && "$status" == "Charging" ]]; then
     XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -i $path_img -t $time_limit "Battery fully charged" "Unplug power"
-    elif [[ "$battery" -gt "$upper_limit" ]]; then
+    elif [[ "$battery" -gt "$upper_limit" && "$status" == "Charging" ]]; then
     XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -i $path_img -t $time_limit  "Battery beyond current limit of $upper_limit" "Unplug power"
-    elif  [[ "$battery" -lt "$lower_limit" ]]; then
-    XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -i $path_img -t $time_limit "Battery less than lower limit of $lower_limit"  "Plug In power"
-    elif [[ "$battery" -lt 25 ]]; then
-    XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -i $path_img -t $time_limit  "Battery less than 25%" "Plug In power"
-    elif [[ "$battery" -lt 10 ]]; then
+    elif [[ "$battery" -lt 10 && "$status" == "Discharging" ]]; then
     XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -i $path_img -t $time_limit "Battery critically low." "Plug In Immediately."
+    elif [[ "$battery" -lt 25 && "$status" == "Discharging" ]]; then
+    XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -i $path_img -t $time_limit  "Battery less than 25%" "Plug In power"
+    elif  [[ "$battery" -lt "$lower_limit" && "$status" == "Discharging" ]]; then
+    XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -i $path_img -t $time_limit "Battery less than lower limit of $lower_limit"  "Plug In power"
 fi
